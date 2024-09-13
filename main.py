c7296bb29e113a60581add92f661d1a7071b0916
HitaloPa <patriciohitalo669@gmail.com>
2024-09-13 09:53:52 -0300

    Added Euro Exchange rate and Bitcoin Exchange rate

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ ex_rate = ex_rate.json()
 
 #print(ex_rate)
 dolar_rate = float(ex_rate['USDBRL']['bid'])
+euro_rate = float(ex_rate['EURBRL']['bid'])
+bitcoin_rate = float(ex_rate['BTCBRL']['bid'])
 print(f"O dolar esta custando R${round(dolar_rate,2)}")
-
+print(f"O euro esta custando R${round(euro_rate,2)}")
+print(f"O Bitcoin esta custando R${round(bitcoin_rate,2)}")
