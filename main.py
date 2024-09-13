147348780cc26b0d69c5bc796ce7fef135195b9e
HitaloPa <patriciohitalo669@gmail.com>
2024-09-13 09:48:28 -0300

    Add the comunication with the api of awesome api cotation

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import json
+import requests 
+#TODO Make this Object Orientated
+#TODO Make a Graphic interface with Tkinter
+ex_rate = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+ex_rate = ex_rate.json()
+
+#print(ex_rate)
+dolar_rate = float(ex_rate['USDBRL']['bid'])
+print(f"O dolar esta custando R${round(dolar_rate,2)}")
 
